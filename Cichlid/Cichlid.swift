9e8f5fa4d551b89246c85daa44e598d9511fa8b9
Toshihiro Morimoto <dealforest.net@gmail.com>
2016-02-29 00:04:58 +0900

    first commit

---
+++ Cichlid/Cichlid.swift
@@ -0,0 +1,48 @@
+//
+//  Cichlid.swift
+//
+//  Created by Toshihiro Morimoto on 2/29/16.
+//  Copyright © 2016 Toshihiro Morimoto. All rights reserved.
+//
+
+import AppKit
+
+var sharedPlugin: Cichlid?
+
+class Cichlid: NSObject {
+    var bundle: NSBundle
+
+    class func pluginDidLoad(bundle: NSBundle) {
+        let appName = NSBundle.mainBundle().infoDictionary?["CFBundleName"] as? NSString
+        if appName == "Xcode" {
+            sharedPlugin = Cichlid(bundle: bundle)
+        }
+    }
+
+    init(bundle: NSBundle) {
+        self.bundle = bundle
+
+        super.init()
+        createMenuItems()
+    }
+
+    deinit {
+        NSNotificationCenter.defaultCenter().removeObserver(self)
+    }
+
+    func createMenuItems() {
+        var item = NSApp.mainMenu!!.itemWithTitle("Edit")
+        if item != nil {
+            var actionMenuItem = NSMenuItem(title:"Do Action", action:"doMenuAction", keyEquivalent:"")
+            actionMenuItem.target = self
+            item!.submenu!.addItem(NSMenuItem.separatorItem())
+            item!.submenu!.addItem(actionMenuItem)
+        }
+    }
+
+    func doMenuAction() {
+        let error = NSError(domain: "Hello World!", code:42, userInfo:nil)
+        NSAlert(error: error).runModal()
+    }
+}
+
---
+++ README.md
@@ -0,0 +1,21 @@
+![Cichlid](https://raw.githubusercontent.com/dealforest/Cichlid/master/images/cichlid.png)
+
+## Feature
+
+## Usage
+
+---
+
+## Contact
+
+### Creator
+
+- [Toshihiro Morimoto](http://github.com/dealforest) ([@dealforest](https://twitter.com/dealforest))
+
+### Changes
+
+See [Releases](https://github.com/dealforest/Cichlid/releases).
+
+### License
+
+Tuna is released under the MIT license. See LICENSE for details.
